2f33e9452505a05a17a782abb1268a047f33da43
BerkaySimsek <berkayysimsekk@gmail.com>
2025-09-24 02:00:20 +0300

    Add project files.

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using CarBook.Application.Features.CQRS.Commands.AboutCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _aboutRepository;
+        public UpdateAboutCommandHandler(IRepository<About> aboutRepository)
+        {
+            _aboutRepository = aboutRepository;
+        }
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var value = await _aboutRepository.GetByIdAsync(command.AboutId);
+            value.Title = command.Title;
+            value.Description = command.Description;
+            value.ImageUrl = command.ImageUrl;
+            await _aboutRepository.UpdateAsync(value);
+        }
+    }
+}
---
+++ Presentation/CarBook.WebApi/Controllers/AboutsController.cs
@@ -0,0 +1,62 @@
+﻿using CarBook.Application.Features.CQRS.Commands.AboutCommands;
+using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Features.CQRS.Queries.AboutQueries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutsController : ControllerBase
+    {
+        private readonly CreateAboutCommandHandler _createAboutCommandHandler;
+        private readonly GetAboutQueryHandler _getAboutQueryHandler;
+        private readonly GetAboutByIdQueryHandler _getAboutByIdQueryHandler;
+        private readonly UpdateAboutCommandHandler _updateAboutCommandHandler;
+        private readonly RemoveAboutCommandHandler _deleteAboutCommandHandler;
+
+        public AboutsController(CreateAboutCommandHandler createAboutCommandHandler, GetAboutQueryHandler getAboutQueryHandler, GetAboutByIdQueryHandler getAboutByIdQueryHandler, UpdateAboutCommandHandler updateAboutCommandHandler, RemoveAboutCommandHandler deleteAboutCommandHandler)
+        {
+            _createAboutCommandHandler = createAboutCommandHandler;
+            _getAboutQueryHandler = getAboutQueryHandler;
+            _getAboutByIdQueryHandler = getAboutByIdQueryHandler;
+            _updateAboutCommandHandler = updateAboutCommandHandler;
+            _deleteAboutCommandHandler = deleteAboutCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutList()
+        {
+            var values = await _getAboutQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAbout(int id)
+        {
+            var value=await _getAboutByIdQueryHandler.Handle(new GetAboutByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutCommand command)
+        {
+            await _createAboutCommandHandler.Handle(command);
+            return Ok("Hakkımda Bilgisi Eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveAbout(int id)
+        {
+            await _deleteAboutCommandHandler.Handle(new RemoveAboutCommand(id));
+            return Ok("Hakkımda Bilgisi Silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutCommand command)
+        {
+            await _updateAboutCommandHandler.Handle(command);
+            return Ok("Hakkımda Bilgisi Güncellendi.");
+        }
+    }
+}
