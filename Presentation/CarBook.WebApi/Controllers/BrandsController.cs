ea32ab22ea43aed01737eb839a92762d42c5e9d5
BerkaySimsek <berkayysimsekk@gmail.com>
2025-09-25 03:36:44 +0300

    Brand-crud-api added.

---
+++ Presentation/CarBook.WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,62 @@
+﻿using CarBook.Application.Features.CQRS.Commands.BrandCommands;
+using CarBook.Application.Features.CQRS.Handlers.BrandHandlers;
+using CarBook.Application.Features.CQRS.Queries.BrandQueries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly CreateBrandCommandHandler _createBrandCommandHandler;
+        private readonly GetBrandQueryHandler _getBrandQueryHandler;
+        private readonly GetBrandByIdQueryHandler _getBrandByIdQueryHandler;
+        private readonly UpdateBrandCommandHandler _updateBrandCommandHandler;
+        private readonly RemoveBrandCommandHandler _deleteBrandCommandHandler;
+
+        public BrandsController(CreateBrandCommandHandler createBrandCommandHandler, GetBrandQueryHandler getBrandQueryHandler, GetBrandByIdQueryHandler getBrandByIdQueryHandler, UpdateBrandCommandHandler updateBrandCommandHandler, RemoveBrandCommandHandler deleteBrandCommandHandler)
+        {
+            _createBrandCommandHandler = createBrandCommandHandler;
+            _getBrandQueryHandler = getBrandQueryHandler;
+            _getBrandByIdQueryHandler = getBrandByIdQueryHandler;
+            _updateBrandCommandHandler = updateBrandCommandHandler;
+            _deleteBrandCommandHandler = deleteBrandCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BrandList()
+        {
+            var values = await _getBrandQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrand(int id)
+        {
+            var value = await _getBrandByIdQueryHandler.Handle(new GetBrandByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand(CreateBrandCommand command)
+        {
+            await _createBrandCommandHandler.Handle(command);
+            return Ok("Marka Bilgisi Eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveBrand(int id)
+        {
+            await _deleteBrandCommandHandler.Handle(new RemoveBrandCommand(id));
+            return Ok("Marka Bilgisi Silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandCommand command)
+        {
+            await _updateBrandCommandHandler.Handle(command);
+            return Ok("Marka Bilgisi Güncellendi.");
+        }
+    }
+}
