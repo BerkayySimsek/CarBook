dea50a80f65348ef32ca01213bb510ee43c72d6d
BerkaySimsek <berkayysimsekk@gmail.com>
2025-11-04 00:38:44 +0300

    CarController-frontend added.

---
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7241/api/Cars/GetCarWithBrand");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
